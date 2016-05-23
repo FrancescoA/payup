746e9739d1affab4332c4a0718b166b15013fdf2
Franesco Agosti <fra5@duke.edu>
2016-05-22 17:55:54 -0700

    Set up React development environment and now using sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 var path = require('path');
 var webpack = require('webpack');
- 
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var findPath = function(path_name) {
+  return path.join(__dirname, path_name)
+};
+
 module.exports = {
-  entry: './main.js',
+  entry: ['./src/main.js', './src/stylesheets/main.scss'],
   output: { path: __dirname, filename: 'bundle.js' },
   module: {
     loaders: [
@@ -13,7 +18,23 @@ module.exports = {
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.scss$/,
+        include: findPath('src'),
+        loader: ExtractTextPlugin.extract(['css', 'sass'].join('!'))
       }
     ]
   },
+  devServer: {
+    contentBase: findPath('build'),
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  devtool: 'eval-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('main.css')
+  ]
 };
