818da9d51a23896563349e4346993ab68f3bd7e0
Franesco Agosti <fra5@duke.edu>
2016-06-20 13:18:08 -0700

    Now working with front-end boilerplate

--- src/main.js
+++
@@ -1,4 +0,0 @@
-// import './stylesheets/main.scss'
-
-import Hello from './components/hello.jsx';
-import World from './components/world.jsx';
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,71 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-var findPath = function(path_name) {
-  return path.join(__dirname, path_name)
-};
+var rucksack = require('rucksack-css')
+var webpack = require('webpack')
+var path = require('path')
 
 module.exports = {
-  entry: ['./src/main.js', './src/stylesheets/main.scss'],
-  output: { path: __dirname, filename: '/build/bundle.js' },
+  context: path.join(__dirname, './client'),
+  entry: {
+    jsx: './index.js',
+    html: './index.html',
+    vendor: [
+      'react',
+      'react-dom',
+      'react-redux',
+      'react-router',
+      'react-router-redux',
+      'redux'
+    ]
+  },
+  output: {
+    path: path.join(__dirname, './static'),
+    filename: 'bundle.js',
+  },
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
       },
       {
-        test: /\.scss$/,
-        include: findPath('src'),
-        loader: ExtractTextPlugin.extract(['css', 'sass'].join('!'))
-      }
-    ]
+        test: /\.css$/,
+        include: /client/,
+        loaders: [
+          'style-loader',
+          'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        exclude: /client/,
+        loader: 'style!css'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: [
+          'react-hot',
+          'babel-loader'
+        ]
+      },
+    ],
   },
-  devServer: {
-    contentBase: findPath('templates'),
-    hot: true,
-    inline: true,
-    progress: true
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   },
-  devtool: 'eval-source-map',
+  postcss: [
+    rucksack({
+      autoprefixer: true
+    })
+  ],
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin('main.css')
-  ]
-};
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') }
+    })
+  ],
+  devServer: {
+    contentBase: './client',
+    hot: true
+  }
+}
