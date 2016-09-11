5bb41285cebd270b139790489db2959e89f8475e
Franesco Agosti <fra5@duke.edu>
2016-09-11 16:59:33 -0700

    Added foundation for auth, need to build auth page

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,11 +1,12 @@
-
 import { routerReducer as routing } from 'react-router-redux'
 import { combineReducers } from 'redux'
 import listings from './listings'
+import auth from './auth'
 
 export default combineReducers({
   routing,
-  listings
+  listings,
+  auth
 })
 
 // We would add an auth reducer as well. 
